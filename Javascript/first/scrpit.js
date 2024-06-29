dad1c138b1bac1f11d3af5a329d24e5fa87432b0
Gaurav <royg48848@gmail.com>
2024-06-29 20:28:33 +0530

    javascript first atempt

---
+++ Javascript/first/scrpit.js
@@ -0,0 +1,8 @@
+console.log('Hello World');
+console.log('Hello World test');
+let a = 10;
+console.log(a);
+let $dollar= 20;
+let _underscore = 10;
+console.log($dollar);
+console.log(_underscore);
